e1577e7576e14432fcbfd8c50ea076e5590bfa53
virginiadragan <32567359+virginiadragan@users.noreply.github.com>
2019-03-04 09:34:08 +0200

    Add files via upload
    last commit

--- lucrare_POO/main.js
+++ lucrare_POO/main.js
@@ -1,20 +1,20 @@
 window.onload = function(){
 	let worker1 = new Worker('Ion', 'Creanga', 10, 176);
 	let worker2 = new Worker('Mihai', 'Eminescu', 17, 100);
-	console.log("Lucratorul nr. 1:")
-	console.log(worker1.name);
-	console.log(worker1.surname);
-	console.log(worker1.hourRate);
-	console.log(worker1.hours);
-	console.log(worker1.getSalary());
-	console.log("Lucratorul nr. 2:")
-	console.log(worker2.name);
-	console.log(worker2.surname);
-	console.log(worker2.hourRate);
-	console.log(worker2.hours);
-	console.log(worker2.getSalary());
+	let worker3 = new Worker('Alecu', 'Russo', 15, 200);
+	let workers = [worker1, worker2, worker3];
+	for(let i = 0; i < workers.length; i++){
+		console.log(workers[i].getName());
+		console.log(workers[i].getSurname());
+		console.log(workers[i].getRate());
+		console.log(workers[i].getHours());
+		workers[i].increaseSalary(2);
+		console.log(workers[i].getSalary());
+		workers[i].setTax(18);
+		console.log(workers[i].getSalaryNetto(18));
+	}
+
 	let maxSalary = worker1.getSalary()>worker2.getSalary()?worker1.name:worker2.name;
-	let salary = 
 	console.log("Lucratorul cu salariul mai mare este: ", maxSalary, ". Salariul este mai mare cu: " , Math.abs(worker2.getSalary() - worker1.getSalary()));
 }
 
@@ -26,10 +26,30 @@ class Worker{
 		this.hourRate = hourRate;
 		this.hours =hours;
 	}
-	getSalary(){
-		return this.hourRate*this.hours; //+ " = " + this.hourRate + " + " + this.hours;
-
+	getName(){
+		return this.name;
+	}
+	getSurname(){
+		return this.surname;
+	}
+	getRate(){
+		return this.hourRate;
+	}
+	getHours(){
+		return this.hours;
 	}
 
+	getSalary(){
+		return this.hourRate*this.hours; //+ " = " + this.hourRate + " * " + this.hours;
+	}
+	increaseSalary(i){
+		this.hourRate += i;
+	}
+	setTax(i){
+		return (this.getSalary()*i)/100;
+	}
+	getSalaryNetto(i){
+		return (this.getSalary() - this.setTax(i));
+	}
 
 }
